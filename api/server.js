cf64861ad88bf9a88f49e7c21f4e5ba0497c79ac
Potnuru Santhoshini <potnurusanthoshini@gmail.com>
2026-02-16 20:47:28 +0530

    Fix Vercel 500 error by using in-memory database

--- api/server.js
+++ api/server.js
@@ -1,14 +1,22 @@
 const jsonServer = require('json-server');
+const fs = require('fs');
 const path = require('path');
+
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
+// Load the database from db.json into memory
+// This avoids writing to the read-only filesystem on Vercel
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+const router = jsonServer.router(db);
+
 server.use(middlewares);
+
 // Re-route /api/* requests to the router
 server.use(jsonServer.rewriter({
     '/api/*': '/$1'
 }));
+
 server.use(router);
 
 module.exports = server;
